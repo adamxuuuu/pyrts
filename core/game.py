e0370b48d116cda2c78578f2ac1a709c8da4b49e
adamx <adamxu@awesomego.com.cn>
2023-02-28 15:27:20 +0800

    Optimize game logic

--- core/game.py
+++ core/game.py
@@ -1,13 +1,10 @@
 import random
 import pygame
 from pygame.locals import *
-from pygame.math import Vector2
 
 from entity.ball import Ball
-from entity.tile import Tile, TileType
 from core.grid import Grid
 from util.constants import *
-from util.spritesheet import SpriteSheet
 
 
 class Game:
@@ -15,26 +12,13 @@ class Game:
     def __init__(self):
         # Set up the game window
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption('RTS Game')
-
-        SS = SpriteSheet(
-            "sprites/environment/MASGrassLand/Dawnbringer-16/MAS-Tileset-DB.png")
-
-        # Create background
-        self.background = pygame.Surface(self.screen.get_size())
-        self.background = self.background.convert()
-        grid = Grid(SS, self.background)
+        self.grid = Grid()
 
         # Create game objects
         self.balls = pygame.sprite.Group()
         for _ in range(100):
             _b = Ball(random.randrange(10, 790), random.randrange(10, 590))
             self.balls.add(_b)
-
-        # Blit everything to the screen
-        self.screen.blit(self.background, (0, 0))
-        pygame.display.flip()
 
         # Set up the clock
         self.clock = pygame.time.Clock()
@@ -72,6 +56,5 @@ class Game:
         self.balls.update(self.dt)
 
     def __render__(self):
-        self.balls.clear(self.screen, self.background)
-        self.balls.draw(self.screen)
+        self.grid.draw(self.balls)
         pygame.display.flip()
--- core/grid.py
+++ core/grid.py
@@ -1,20 +1,40 @@
 from util.constants import *
+from util.spritesheet import SpriteSheet
 from entity.tile import Tile, TileType
 
 import pygame
 
 class Grid():
 
-    def __init__(self, SS, background):
+    def __init__(self):
+        # Create screen
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('RTS Game')
+
+        # Load spritesheet
+        self.SS = SpriteSheet(
+            "sprites/environment/MASGrassLand/Dawnbringer-16/MAS-Tileset-DB.png")
         self.grid = {}
         self.tiles = pygame.sprite.Group()
 
+        # Create background
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+
         for y in range(0, 600, TILE_SIZE_LARGE):
             for x in range(0, 800, TILE_SIZE_LARGE):
-                t = Tile(x, y, TileType.GRASS_32x32, SS)
+                t = Tile(x, y, TileType.GRASS_32x32, self.SS)
                 self.tiles.add(t)
                 self.grid[(x, y)] = []
-                background.blit(t.image, t.rect)
+        
+        # Blit everything to the screen
+        self.background.blits([(spr.image, spr.rect) for spr in self.tiles.sprites()])
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
+
+    def draw(self, gos):
+        gos.clear(self.screen, self.background)
+        gos.draw(self.screen)
 
     def get_coord(self, x, y) -> list:
         return self.grid.get((x, y))
